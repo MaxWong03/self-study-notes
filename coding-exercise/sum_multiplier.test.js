08431e01ad413ce290a40cd23eb241e3f759deaf
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-19 14:35:23 -0800

    test helper functions

--- coding-exercise/sum_multiplier.test.js
+++ coding-exercise/sum_multiplier.test.js
@@ -1,5 +1,8 @@
-const doubleSum = require('./sum_multiplier'); 
+const { doubleSum } = require('./sum_multiplier');
 
 describe('doubleSum', () => {
-
+  test('returns 84 for arr = [2, 5, 6, -6, 16, 2, 3, 6, 5, 3]', () => {
+    const arr = [2, 5, 6, -6, 16, 2, 3, 6, 5, 3];
+    expect(doubleSum(arr)).toEqual(84);
+  });
 })
