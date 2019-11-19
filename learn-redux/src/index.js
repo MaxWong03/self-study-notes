d8e1aa7415bee5b13a1291a274f63a0b2305e21a
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-18 21:15:32 -0800

    extract states to different reducer files

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -4,63 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import allReducer from './reducers'
 
-/**
- * STORE
- * 
- * Store is a globalized state, holds all data / state for your app
- * 
- */
-
-
-/**
- * ACTION
- * 
- * Action describes what you are going to do to
- * 
- * Action is a function that returns an object
- */
-
-const increment = () => {
-  return {
-    type: 'INCREMENT' //name of the action
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT' //name of the action
-  }
-}
-
-/**
- * REDUCER
- * 
- * Reducer describes how actions transform the current state to the next state
-*/
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-const store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-/**
-  * DISPATCH
-  * 
-  * Dispatch executes the action to the reducer
-*/
-store.dispatch(increment());
+const store = createStore(allReducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
