2a0f93a9719564d7834e9c7c61c387bdc14ebb0d
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-19 14:44:20 -0800

    brute force solution

--- coding-exercise/sum_multiplier.js
+++ coding-exercise/sum_multiplier.js
@@ -10,7 +10,16 @@ const reducer = (accumlator, currentValue) => accumlator + currentValue;
 const doubleSum = arr => arr.reduce(reducer) * 2;
 
 const SumMultiplier = arr => {
+  const targetVal = doubleSum(arr);
+  for (let i = 0; i < arr.length - 1; i++) {
+    let num1 = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      let num2 = arr[j];
+      if (num1 * num2 > targetVal) return true
+    }
+  }
 
+  return false;
 }
 
 module.exports = {
