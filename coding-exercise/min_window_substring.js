f1a28cc7886d695cd2736a64d15860dd3a5566c3
MaxWong03 <madmaxmmajournal@gmail.com>
2019-11-20 11:40:48 -0800

    remove irrelavant code and test case

--- coding-exercise/min_window_substring.js
+++ coding-exercise/min_window_substring.js
@@ -6,81 +6,15 @@
 Another example: if strArr is ["aabdccdbcacd", "aad"] then the smallest substring of N that contains all of the characters in K is "aabd" which is located at the beginning of the string. Both parameters will be strings ranging in length from 1 to 50 characters and all of K's characters will exist somewhere in the string N. Both strings will only contains lowercase alphabetic characters.
  */
 
-const getHashPattern = pattern => {
-  const hashPattern = {};
-  for (let i = 0; i < pattern.length; i++) {
-    const currentPatternChar = pattern[i];
-    if (!hashPattern[currentPatternChar]) hashPattern[currentPatternChar] = 1; //if this is the first time encountering that char in pattern
-    else hashPattern[currentPatternChar] += 1; // if it isnt the first time
-  }
-  return hashPattern;
+const getSubString = str => {
+
 }
-
-const getHashText = text => {
-  const hashText = {};
-  for (let i = 0; i < text.length; i++) {
-    const currentTextChar = text[i];
-    if (!hashText[currentTextChar]) hashText[currentTextChar] = 0;
-  }
-  return hashText;
-}
-
-// const MinWindowSubString = strArr => {
-//   const [text, pattern] = strArr;
-//   const hashText = getHashText(text);
-//   const hashPattern = getHashPattern(pattern);
-//   let count = 0;
-//   let left = 0;
-//   let right = text.length - 1;
-
-//   //filling up hashText
-//   for (let i = 0; i < text.length; i++) {
-//     const currentChar = text[i];
-//     if (!hashPattern[currentChar]) hashText[currentChar] += 1; //if currentChar is not part of the pattern, increase its value in hashText
-//     else { //if its part of the pattern
-//       //and its value in hashText has not yet exceed the required occurence in hashPattern
-//       if (hashText[currentChar] <= hashPattern[currentChar]) {
-//         hashText[currentChar] += 1;
-//         count++;
-//       } else { //its value exceed the required occurence in hashPattern
-//         hashText[currentChar] += 1;
-//       }
-//     }
-//   }
-
-//   //finding left indices for substring
-//   for (let i = 0; i < text.length; i++) {
-//     const currentChar = text[i];
-//     if (!hashPattern[currentChar]) { //if currentChar is not part of the pattern
-//       left += 1; //move the left indice by 1
-//       hashText[currentChar] -= 1; //decrease its value in hashText by 1
-//     } else { //if its part of the pattern
-//       //and we could shift the left indices and still have enough characters to satisfiy the pattern
-//       if (hashText[currentChar] > hashPattern[currentChar]) {
-//         left += 1;
-//         hashText[currentChar] -= 1;
-//       }
-//     }
-//   }
-
-//   return text.substring(left)
-
-// }
-
-// ["aaabaaddae", "aed"]
-// hashPattern = { a: 1, e: 1, d: 1 }
-// hashText = { a: 6, b: 1, d: 2, e: 1 }
-// left = 0
-
-// ["aabdccdbcacd", "aad"]
-// hashPattern = { a: 2, d: 1 }
-// hashText = { a: 3, b: 2, d: 3, c: 4 }
-// left = 0
 
 
 const MinWindowSubString = strArr => {
   const [text, pattern] = strArr;
   let [left, right] = [0, 0]; //init the pointers
+
 }
 
 module.exports = {
--- coding-exercise/min_window_substring.test.js
+++ coding-exercise/min_window_substring.test.js
@@ -1,31 +1,5 @@
 const { getHashPattern, getHashText, MinWindowSubString } = require('./min_window_substring');
 
-describe('getHashPattern', () => {
-  test('returns {a: 1, e: 1, d: 1} for pattern = "aed"', () => {
-    const hashPattern = getHashPattern('aed');
-    const expected = { a: 1, e: 1, d: 1 };
-    expect(hashPattern).toEqual(expected);
-  });
-
-  test('returns {a: 2, d: 1} for pattern = "aad"', () => {
-    const hashPattern = getHashPattern('aad');
-    const expected = { a: 2, d: 1 };
-    expect(hashPattern).toEqual(expected);
-  });
-});
-
-describe('getHashText', () => {
-  test('returns {a: 0, b: 0, d: 0, e: 0} for text = "aaabaaddae"', () => {
-    const hashText = getHashText('aaabaaddae');
-    const expected = { a: 0, b: 0, d: 0, e: 0 };
-    expect(hashText).toEqual(expected);
-  });
-  test('returns {a: 0, b: 0, d: 0, c: 0} for text = "aabdccdbcacd"', () => {
-    const hashText = getHashText('aabdccdbcacd');
-    const expected = { a: 0, b: 0, d: 0, c: 0 };
-    expect(hashText).toEqual(expected);
-  });
-});
 
 describe('MinWindowSubString', () => {
   test('returns dae when strArr is ["aaabaaddae", "aed"]', () => {
