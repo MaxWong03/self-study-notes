01293624463a78751a237fde8b358c6b48e75c95
Max Wong <max@techsamurais.com>
2019-12-02 16:22:30 -0800

    write additional test case

--- coding-exercise/two-sum.test.js
+++ coding-exercise/two-sum.test.js
@@ -1,6 +1,13 @@
 const { twoSum } = require('./two-sum');
 
 describe('twoSum', () => {
+  test('returns an array', () => {
+    const numArr = [2, 7, 11, 15];
+    const target = 9;
+    const result = twoSum(numArr, target);
+    expect(Array.isArray(result)).toBeTruthy();
+  });
+
   test('returns [0, 1] given numArr = [2, 7, 11, 15] and target = 9', () => {
     const numArr = [2, 7, 11, 15];
     const target = 9;
