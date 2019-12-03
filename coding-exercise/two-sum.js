e2ea3a5f51391f003accda123ef456743acdc92f
Max Wong <max@techsamurais.com>
2019-12-02 16:20:33 -0800

    solve by brute force approach

--- coding-exercise/two-sum.js
+++ coding-exercise/two-sum.js
@@ -5,9 +5,15 @@
  */
 
 const twoSum = (numArr, target) => {
-
+  for (let i = 0; i < numArr.length; i++) {
+    const num1  = numArr[i];
+    for (let j = i + 1; j < numArr.length; j++) {
+      const num2 = numArr[j];
+      if (num1 + num2 === target) return [i, j];
+    }
+  }
 };
 
 module.exports = {
   twoSum
-}
+};
