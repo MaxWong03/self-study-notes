740f51ea04e7aa2d8a3a4a321fc95161d82bc13d
Max Wong <max@techsamurais.com>
2019-12-02 16:24:57 -0800

    refactor test cases

--- coding-exercise/two-sum.test.js
+++ coding-exercise/two-sum.test.js
@@ -1,23 +1,20 @@
 const { twoSum } = require('./two-sum');
 
 describe('twoSum', () => {
+  
+  const numArr = [2, 7, 11, 15];
+  const target = 9;
+  const result = twoSum(numArr, target);
+
   test('returns an array', () => {
-    const numArr = [2, 7, 11, 15];
-    const target = 9;
-    const result = twoSum(numArr, target);
     expect(Array.isArray(result)).toBeTruthy();
   });
 
   test('returns an array of length 2', () => {
-    const numArr = [2, 7, 11, 15];
-    const target = 9;
-    const result = twoSum(numArr, target);
     expect(result.length).toEqual(2);
   });
 
   test('returns [0, 1] given numArr = [2, 7, 11, 15] and target = 9', () => {
-    const numArr = [2, 7, 11, 15];
-    const target = 9;
-    expect(twoSum(numArr, target)).toEqual([0, 1]);
+    expect(result).toEqual([0, 1]);
   });
 });
