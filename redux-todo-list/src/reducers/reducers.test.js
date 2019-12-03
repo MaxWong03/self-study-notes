2b27eddd30534e6cd76b1c400f5e8420635051b8
Max Wong <max@techsamurais.com>
2019-12-03 15:44:54 -0800

    write test for default reducer action

--- redux-todo-list/src/reducers/reducers.test.js
+++ redux-todo-list/src/reducers/reducers.test.js
@@ -5,8 +5,16 @@ describe('todoReducer', () => {
   it('should return initial state = [] for unknown action', () => {
     const returnState = todoReducer(undefined, { type: 'Unknown' });
     const expectedState = [];
+    expect(returnState.length).toEqual(0);
     expect(returnState).toEqual(expectedState);
-  })
+  });
+  
+  it('should return current state for unknown action', () => {
+    const currentState = [{id: 5, name: 'Task', isCompleted: false}];
+    const returnState = todoReducer(currentState, { type: 'Unknown' });
+    expect(returnState.length).toEqual(1);
+    expect(returnState).toEqual(currentState);
+  });
 });
 
 
