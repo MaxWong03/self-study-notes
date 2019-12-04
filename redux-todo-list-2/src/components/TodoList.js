0463a5327dd28cd87fa0da47eefdfac6038408a6
Max Wong <max@techsamurais.com>
2019-12-04 10:52:32 -0800

    build TodoList component

---
+++ redux-todo-list-2/src/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Todo from './Todo';
+
+export default function TodoList({ todos, onTodoClick }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <Todo
+          key={index}
+          {...todo}
+          onClick={() => onTodoClick(index)}
+        />
+      ))}
+    </ul>
+  )
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  onTodoClick: PropTypes.func.isRequired
+}
