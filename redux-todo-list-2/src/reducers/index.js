82661b084b7840418926d29a170510dd8dcc6b23
Max Wong <max@techsamurais.com>
2019-12-04 15:12:59 -0800

    finish todo reducer

--- redux-todo-list-2/src/reducers/index.js
+++ redux-todo-list-2/src/reducers/index.js
@@ -7,7 +7,7 @@ import {
 } from '../actions'
 const { SHOW_ALL } = VisibilityFilters;
 
-const visibilityFilter = (state = SHOW_ALL, action) => {
+export const visibilityFilter = (state = SHOW_ALL, action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return action.filter;
@@ -16,7 +16,7 @@ const visibilityFilter = (state = SHOW_ALL, action) => {
   }
 }
 
-const todos = (state = [], action) => {
+export const todos = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       return [
---
+++ redux-todo-list-2/src/reducers/index.test.js
@@ -0,0 +1,78 @@
+import { todos, visibilityFilter } from './index';
+import { addTodo, toggleTodo, setVisibilityFilter } from '../actions'
+
+expect.extend({
+  toContainObject(received, argument) {
+
+    const pass = this.equals(received,
+      expect.arrayContaining([
+        expect.objectContaining(argument)
+      ])
+    )
+
+    if (pass) {
+      return {
+        message: () => (`expected ${this.utils.printReceived(received)} not to contain object ${this.utils.printExpected(argument)}`),
+        pass: true
+      }
+    } else {
+      return {
+        message: () => (`expected ${this.utils.printReceived(received)} to contain object ${this.utils.printExpected(argument)}`),
+        pass: false
+      }
+    }
+  }
+})
+
+describe('todoApp reducer', () => {
+  describe('todos reducer', () => {
+    it('should handle ADD_TODO action correctly', () => {
+      const currentState = [];
+      const addTodoObj1 = addTodo('New Task 1');
+      let nextState = todos(currentState, addTodoObj1);
+      expect(nextState.length).toEqual(1);
+      expect(nextState).toContainObject({
+        text: 'New Task 1',
+        completed: false,
+        id: 0
+      })
+      const addTodoObj2 = addTodo('New Task 2');
+      nextState = todos(nextState, addTodoObj2);
+      expect(nextState.length).toEqual(2);
+      expect(nextState).toContainObject({
+        text: 'New Task 2',
+        completed: false,
+        id: 1
+      });
+      const addTodoObj3 = addTodo('New Task 3');
+      nextState = todos(nextState, addTodoObj3);
+      expect(nextState.length).toEqual(3);
+      expect(nextState).toContainObject({
+        text: 'New Task 3',
+        completed: false,
+        id: 2
+      });
+    });
+
+    it('should handle TOGGLE_TODO action correctly', () => {
+      const currentState = [];
+      const addTodoObj4 = addTodo('New Task 4');
+      const addTodoObj5 = addTodo('New Task 5');
+      let nextState = todos(currentState, addTodoObj4);
+      nextState = todos(nextState, addTodoObj5);
+      const toggleTodoObj = toggleTodo(4);
+      nextState = todos(nextState, toggleTodoObj);
+      expect(nextState).toContainObject({
+        text: 'New Task 5',
+        completed: true,
+        id: 4
+      });
+      nextState = todos(nextState, toggleTodoObj);
+      expect(nextState).toContainObject({
+        text: 'New Task 5',
+        completed: false,
+        id: 4
+      })
+    });
+  });
+});
