0a85231b6466b7bfcf7ce41a127d93387b064190
Max Wong <max@techsamurais.com>
2019-12-04 13:57:55 -0800

    finish testing action creators

--- redux-todo-list-2/src/actions/index.test.js
+++ redux-todo-list-2/src/actions/index.test.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, addTodo, toggleTodo, setVisibilityFilter } from './index';
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './index';
 
 describe('todo actions', () => {
   describe('#addTodo', () => {
@@ -40,6 +40,26 @@ describe('todo actions', () => {
       const todoObj2 = toggleTodo(31);
       expect(todoObj1.id).toEqual(1);
       expect(todoObj2.id).toEqual(31);
+    });
+  });
+
+  describe('#setVisibilityFilter', () => {
+    it('should return object with type = SET_VISIBILITY_FILTER, filter as key', () => {
+      const todoObj = setVisibilityFilter(VisibilityFilters.SHOW_ACTIVE);
+      const todoObjKeys = Object.keys(todoObj);
+      expect(todoObj.type).toEqual(SET_VISIBILITY_FILTER);
+      expect(todoObjKeys.length).toEqual(2);
+      expect(todoObjKeys).toContain('type');
+      expect(todoObjKeys).toContain('filter');
+    });
+
+    it('should return object with filter value equal to the given param', () => {
+      const todoObjAll = setVisibilityFilter(VisibilityFilters.SHOW_ALL);
+      const todoObjCompleted = setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED);
+      const todoObjActive = setVisibilityFilter(VisibilityFilters.SHOW_ACTIVE);
+      expect(todoObjAll.filter).toEqual(VisibilityFilters.SHOW_ALL);
+      expect(todoObjCompleted.filter).toEqual(VisibilityFilters.SHOW_COMPLETED);
+      expect(todoObjActive.filter).toEqual(VisibilityFilters.SHOW_ACTIVE);
     });
   });
 });
