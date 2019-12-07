87b6f607c5ad48599406cf2879eeea9dbce43c31
Max Wong <max@techsamurais.com>
2019-12-06 16:18:38 -0800

    refactor asyncApp using less verbose syntax

--- Async-Redux/reddit-api/src/components/Picker.test.js
+++ Async-Redux/reddit-api/src/components/Picker.test.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import Picker from './Picker';
-
-const mockStore = configureStore([]);
 
 afterEach(cleanup);
 
 describe('Picker Component', () => {
-  let store;
-  
-  beforeEach(() => {
-    store = mockStore({
-      
-    })
-  });
-
+ 
   it('renders without crashing', () => {
     const fn = jest.fn();
     render(
@@ -28,7 +17,4 @@ describe('Picker Component', () => {
     );
   });
 
-  it('changes value when onChange function is called', () => {
-
-  });
 });
