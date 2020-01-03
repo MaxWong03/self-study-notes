a479e8cbff4aff005a9774c9e9e6784111879ac1
Max Wong <max@techsamurais.com>
2020-01-03 10:31:34 -0800

    created jafar for testing

---
+++ TypeScript/Disney_Villainous_OOP/Villain/Jafar.ts
@@ -0,0 +1,21 @@
+import Villain = require('./Villain');
+import RealmLocation = require('../RealmLocation');
+import Action = require('../Action/Action');
+
+class Jafar extends Villain {
+
+  name: string = 'Jafar';
+  objective: string = "Start your turn with Magic Lamp at the Sultan's Palace and have Genie on your side";
+  realm: Array<RealmLocation> = [
+    new RealmLocation('Sultan Palace', false, [new Action('Play A Card'), new Action('Activate')], [new Action('Vanquish'), new Action('Fate')]),
+    new RealmLocation('Streets Of Agrabah', false, [new Action('GainPower'), new Action('Fate')], [new Action('Discard'), new Action('PlayACard')]),
+    new RealmLocation('Oasis', false, [new Action('Activate'), new Action('PlayACard')], [new Action('GainPower'), new Action('PlayACard')]),
+    new RealmLocation('Cave Of Wonders', true, [new Action('Discard'), new Action('GainPower')], [new Action('PlayACard'), new Action('MoveAllyOrItem')])
+  ]
+
+  constructor(name: string, objective: string, realm: Array<RealmLocation>) {
+    super(name, objective, realm);
+  }
+}
+
+export = Jafar;
--- TypeScript/Disney_Villainous_OOP/Villain/Villain.ts
+++ TypeScript/Disney_Villainous_OOP/Villain/Villain.ts
@@ -1,5 +1,4 @@
 import RealmLocation = require('../RealmLocation');
-import Action = require('../Action/Action');
 
 class Villain {
   name: string;
@@ -13,22 +12,5 @@ class Villain {
   }
 }
 
-const GainPower = new Action('Gain Power');
-const Vanquish = new Action('Vanquish');
-const Fate = new Action('Fate');
-const MoveAllyOrItem = new Action('MoveAllyOrItem');
-const Discard = new Action ('Discard');
-const Activate = new Action ('Activate');
-const PlayACard = new Action('Play A Card');
 
-const SultanPalace = new RealmLocation('Sultan Palace', false, [PlayACard, Activate], [Vanquish, Fate]);
-const StreetsOfAgrabah = new RealmLocation('Streets Of Agrabah', false, [GainPower, Fate], [Discard, PlayACard]);
-const Oasis = new RealmLocation('Oasis', false, [Activate, PlayACard], [GainPower, PlayACard]);
-const CaveOfWonders = new RealmLocation('Cave Of Wonders', true, [Discard, GainPower], [PlayACard, MoveAllyOrItem]);
-const JafarRealm = [SultanPalace, StreetsOfAgrabah, Oasis, CaveOfWonders];
-
-const Jafar = new Villain('Jafar', "Start your turn with Magic Lamp at the Sultan's Palace and have Genie on your side", JafarRealm);
-
-console.log(Jafar.name);
-console.log(Jafar.objective);
-console.log(Jafar.realm);
+export = Villain;
