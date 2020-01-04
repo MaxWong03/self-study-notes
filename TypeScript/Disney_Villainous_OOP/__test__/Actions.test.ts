aa134531955704a211d49ef5ef0e5bb3f9791c2c
Max Wong <max@techsamurais.com>
2020-01-03 16:23:48 -0800

    finish basic unit testing for Action Classes

--- TypeScript/Disney_Villainous_OOP/__test__/Actions.test.ts
+++ TypeScript/Disney_Villainous_OOP/__test__/Actions.test.ts
@@ -2,6 +2,10 @@ import Action = require('../Action/Action');
 import GainPower = require('../Action/GainPower');
 import PlayCard = require('../Action/PlayCard');
 import Activate = require('../Action/Activate');
+import Vanquish = require('../Action/Vanquish');
+import Fate = require('../Action/Fate');
+import MoveAllyItem = require('../Action/MoveAllyItem');
+import Discard = require('../Action/Discard');
 
 describe('Action', () => {
   test('Any Action is not covered by default', () => {
@@ -24,4 +28,41 @@ describe('Action', () => {
     expect(playACard.name).toBe('Play A Card');
     expect(playACard.targetCard).toBe('');
   });
+
+  test('Activate is not covered and does not play any card by default', () => {
+    const activate = new Activate();
+    expect(activate instanceof Action).toBeTruthy();
+    expect(activate.name).toBe('Activate');
+    expect(activate.targetCard).toBe('');
+  });
+
+  test('Play A Card is not covered and does not play any card by default', () => {
+    const vanquish = new Vanquish();
+    expect(vanquish instanceof Action).toBeTruthy();
+    expect(vanquish.name).toBe('Vanquish');
+    expect(vanquish.targetHero).toBe('');
+  });
+
+  test('Play A Card is not covered and does not play any card by default', () => {
+    const fate = new Fate();
+    expect(fate instanceof Action).toBeTruthy();
+    expect(fate.name).toBe('Fate');
+    expect(fate.targetPlayer).toBe('');
+  });
+
+  test('Play A Card is not covered and does not play any card by default', () => {
+    const moveAllyItem = new MoveAllyItem();
+    expect(moveAllyItem instanceof Action).toBeTruthy();
+    expect(moveAllyItem.name).toBe('Move An Ally Or Item');
+    expect(moveAllyItem.targetAlly).toBe('');
+    expect(moveAllyItem.targetItem).toBe('');
+  });
+
+  test('Play A Card is not covered and does not play any card by default', () => {
+    const discard = new Discard();
+    expect(discard instanceof Action).toBeTruthy();
+    expect(discard.name).toBe('Discard');
+    expect(discard.targetCards).toEqual([]);
+  });
+  
 })
