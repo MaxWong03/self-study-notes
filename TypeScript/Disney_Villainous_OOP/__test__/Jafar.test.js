b419f5c49c803c799e01332adbfb12e0611f71d8
MaxWong03 <madmaxmmajournal@gmail.com>
2020-01-05 14:09:34 -0800

    debug setter method for villain

--- TypeScript/Disney_Villainous_OOP/__test__/Jafar.test.js
+++ TypeScript/Disney_Villainous_OOP/__test__/Jafar.test.js
@@ -4,34 +4,36 @@ var Jafar = require("../Villain/Jafar");
 var Villain = require("../Villain/Villain");
 describe('Jafar', function () {
     test('Attributes', function () {
-        var jafar = new Jafar();
+        var jafar = new Jafar(3);
         expect(jafar instanceof Villain).toBeTruthy();
         expect(jafar.name).toBe('Jafar');
         expect(jafar.realm.length).toBe(4);
         expect(jafar.currentLocation).toBe('Sultan Palace');
-        expect(jafar.power).toBe(0);
-    });
-    test('General Methods', function () {
-        var jafar = new Jafar();
-        expect(jafar.hasLockedLocation()).toBeTruthy();
-        expect(jafar.hasLockedLocation()).not.toBeFalsy();
-    });
-    test('Moving', function () {
-        var jafar = new Jafar();
-        expect(function () { jafar.move(jafar.currentLocation); }).toThrow();
-        jafar.move('Streets Of Agrabah');
-        expect(jafar.currentLocation).toBe('Streets Of Agrabah');
-        expect(jafar.currentLocation).not.toBe('Sultan Palace');
-        expect(function () { jafar.move('Cave Of Wonders'); }).toThrow();
-    });
-    test('Moving to locations with Gain Power Action', function () {
-        var jafar = new Jafar();
-        jafar.move('Streets Of Agrabah');
-        expect(jafar.currentLocation).toBe('Streets Of Agrabah');
-        var currentLocation = jafar.getLocationClass('Streets Of Agrabah');
-        currentLocation.topActions[0].execute(jafar);
-        jafar.power = 3;
-        console.log(jafar.power);
         expect(jafar.power).toBe(3);
     });
+    test('General Methods', function () {
+        var jafar = new Jafar(0);
+        expect(jafar.hasLockedLocation()).toBeTruthy();
+        expect(jafar.hasLockedLocation()).not.toBeFalsy();
+        jafar.power = 3;
+        expect(jafar.power).toBe(3);
+    });
+    // test('Moving', () => {
+    //   const jafar = new Jafar(0);
+    //   expect(() => { jafar.move(jafar.currentLocation) }).toThrow();
+    //   jafar.move('Streets Of Agrabah');
+    //   expect(jafar.currentLocation).toBe('Streets Of Agrabah');
+    //   expect(jafar.currentLocation).not.toBe('Sultan Palace');
+    //   expect(() => { jafar.move('Cave Of Wonders') }).toThrow();
+    // });
+    // test('Moving to locations with Gain Power Action', () => {
+    //   const jafar = new Jafar(0);
+    //   jafar.move('Streets Of Agrabah');
+    //   expect(jafar.currentLocation).toBe('Streets Of Agrabah');
+    //   const currentLocation = jafar.getLocationClass('Streets Of Agrabah');
+    //   currentLocation.topActions[0].execute(jafar);
+    //   jafar.power = 3;
+    //   console.log(jafar.power);
+    //   expect(jafar.power).toBe(3);
+    // })
 });
--- TypeScript/Disney_Villainous_OOP/__test__/Jafar.test.ts
+++ TypeScript/Disney_Villainous_OOP/__test__/Jafar.test.ts
@@ -3,22 +3,24 @@ import Villain = require('../Villain/Villain');
 
 describe('Jafar', () => {
   test('Attributes', () => {
-    const jafar = new Jafar();
+    const jafar = new Jafar(3);
     expect(jafar instanceof Villain).toBeTruthy();
     expect(jafar.name).toBe('Jafar');
     expect(jafar.realm.length).toBe(4);
     expect(jafar.currentLocation).toBe('Sultan Palace');
-    expect(jafar.power).toBe(0);
+    expect(jafar.power).toBe(3);
   });
 
   test('General Methods', () => {
-    const jafar = new Jafar();
+    const jafar = new Jafar(0);
     expect(jafar.hasLockedLocation()).toBeTruthy();
     expect(jafar.hasLockedLocation()).not.toBeFalsy();
+    jafar.power = 3;
+    expect(jafar.power).toBe(3);
   });
 
   test('Moving', () => {
-    const jafar = new Jafar();
+    const jafar = new Jafar(0);
     expect(() => { jafar.move(jafar.currentLocation) }).toThrow();
     jafar.move('Streets Of Agrabah');
     expect(jafar.currentLocation).toBe('Streets Of Agrabah');
@@ -27,14 +29,12 @@ describe('Jafar', () => {
   });
 
   test('Moving to locations with Gain Power Action', () => {
-    const jafar = new Jafar();
+    const jafar = new Jafar(0);
     jafar.move('Streets Of Agrabah');
     expect(jafar.currentLocation).toBe('Streets Of Agrabah');
     const currentLocation = jafar.getLocationClass('Streets Of Agrabah');
     currentLocation.topActions[0].execute(jafar);
-    jafar.power = 3;
-    console.log(jafar.power);
-    expect(jafar.power).toBe(3);
+    expect(jafar.power).toBe(1);
   })
 })
 
