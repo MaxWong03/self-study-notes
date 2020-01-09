79ed3aa7a6254485ebc5b20605d62fbf8dbc9f11
Max Wong <max@techsamurais.com>
2020-01-09 13:30:17 -0800

    convert with-data with hooks

--- Higher-Order-Components/src/with-data.js
+++ Higher-Order-Components/src/with-data.js
@@ -1,35 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const withData = WrappedComponent => {
-  class WithData extends React.Component {
-    constructor(props) {
-      super(props);
+const withData = WrappedComponent => ({ dataSource, ...otherProps }) => {
 
-      this.state = {
-        data: []
-      };
-    }
-
-    componentDidMount() {
-      setTimeout(() => {
-        fetch(this.props.dataSource)
-          .then(response => response.json())
-          .then(data => this.setState({ data: data.slice(0, 3) }));
-      }, 1500);
-    }
-
-    render() {
-      const { dataSource, ...otherProps } = this.props;
-
-      return this.state.data.length < 1 ? (
-        <h1>LOADING</h1>
-      ) : (
-        <WrappedComponent data={this.state.data} {...otherProps} />
-      );
-    }
-  }
-
-  return WithData;
-};
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(dataSource)
+        .then(response => response.json())
+        .then(data => setData(data.slice(0, 3)))
+    }, 1500);
+  });
+  return (
+    data.length < 1
+      ? (<h1>LOADING</h1>)
+      : <WrappedComponent data={data} {...otherProps} />
+  );
+}
 
 export default withData;
+
+
