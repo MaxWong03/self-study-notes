d4b17ba4c7ac4d37aa739c930393d33d83d63b1d
Max Wong <max@techsamurais.com>
2020-01-22 17:25:51 -0800

    finish exercise 4

--- Ruby/ar-exercises/exercises/exercise_4.rb
+++ Ruby/ar-exercises/exercises/exercise_4.rb
@@ -7,3 +7,33 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create({
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false
+  womens_apparel: true,
+})
+
+Store.create({
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true
+  womens_apparel: false,
+})
+
+Store.create({
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true
+  womens_apparel: true,
+})
+
+@men_stores = Store.where(mens_apparel: true)
+
+puts "--- MEN STORES ---"
+@men_stores.each {|store| puts "Name:#{store.name}, Annaul Revenue: #{store.annual_revenue}"}
+
+@poor_women_stores = Store.where(["womens_apparel = ? and annual_revenue <= ?", true, 1000000])
+
+puts "--- WOMEN STORES ---"
+@poor_women_stores.each {|store| puts "Name:#{store.name}, Annaul Revenue: #{store.annual_revenue}"}
