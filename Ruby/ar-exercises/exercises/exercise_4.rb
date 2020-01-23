a8946eef263ffe1c1e1c702dda271d415e8144ae
Max Wong <max@techsamurais.com>
2020-01-22 17:39:46 -0800

    finish exercise 5

--- Ruby/ar-exercises/exercises/exercise_4.rb
+++ Ruby/ar-exercises/exercises/exercise_4.rb
@@ -10,22 +10,22 @@ puts "----------"
 Store.create({
   name: "Surrey",
   annual_revenue: 224000,
-  mens_apparel: false
-  womens_apparel: true,
+  mens_apparel: false,
+  womens_apparel: true
 })
 
 Store.create({
   name: "Whistler",
   annual_revenue: 1900000,
-  mens_apparel: true
-  womens_apparel: false,
+  mens_apparel: true,
+  womens_apparel: false
 })
 
 Store.create({
   name: "Yaletown",
   annual_revenue: 430000,
-  mens_apparel: true
-  womens_apparel: true,
+  mens_apparel: true,
+  womens_apparel: true
 })
 
 @men_stores = Store.where(mens_apparel: true)
--- Ruby/ar-exercises/exercises/exercise_5.rb
+++ Ruby/ar-exercises/exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+
+puts "All the stores have total_revenue of #{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+
+puts "All the stores have average_revenue of #{average_revenue}"
+
+num_of_profitable_stores = Store
+                      .where(["annual_revenue >= ?", 1000000])
+                      .count
+
+puts "There are #{num_of_profitable_stores} that generate more than $1M in annual sales"
